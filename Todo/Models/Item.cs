ace0a0a14fbcadbe8a1582791022acb0a6518188
noppolp <knookcs08@gmail.com>
2015-09-25 15:47:59 +0700

    init

---
+++ Todo/Models/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Todo.Models
+{
+    public class Item
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "desc")]
+        public string Desc { get; set; }
+
+        [JsonProperty(PropertyName = "isComplete")]
+        public string Completed { get; set; }
+    }
+}
